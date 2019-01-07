95d27808fc53edae2dadf675a02f813f38db136e
Stephen Cowley <StephenCowley44@gmail.com>
2019-01-07 16:26:50 -0700

    Added Brackish Clump recipe

--- SkyblockThoriumHelper.cs
+++ SkyblockThoriumHelper.cs
@@ -4,25 +4,44 @@ using Terraria.ID;
 using ThoriumMod;
 
 namespace SkyblockThoriumHelper {
-	class SkyblockThoriumHelper : Mod {
-		public SkyblockThoriumHelper() {
-		}
+    class SkyblockThoriumHelper : Mod {
+        public SkyblockThoriumHelper() {
+        }
         public override void AddRecipeGroups() {
+            RecipeGroup group;
+
+            group = new RecipeGroup(() => Lang.misc[37] + " Basic Fish", new int[]
+            {
+                ItemID.Bass,
+                ItemID.Trout,
+                ItemID.AtlanticCod,
+                ItemID.RedSnapper,
+                ItemID.Salmon,
+                ItemID.Tuna,
+            });
+            RecipeGroup.RegisterGroup("SkyblockThoriumHelper:BasicFish", group);
         }
         public override void AddRecipes() {
-            //if (ModLoader.GetLoadedMods().Contains("ThoriumMod")) {
-                // Get the Thorium mod
-            //    Mod ThoriumMod = ModLoader.GetMod("ThoriumMod");
-
+            ModRecipe recipe;
+            Mod thoriumMod = ModLoader.GetMod("ThoriumMod");
 
             // Make a recipe for Marine Rock
-            ModRecipe recipe = new ModRecipe(ThoriumMod);
-            recipe.AddIngredient(ItemID.GraniteBlock, 25);
+            recipe = new ModRecipe(this);
+            recipe.AddIngredient(ItemID.Granite, 25);
             recipe.AddIngredient(ItemID.SandBlock, 25);
             recipe.AddIngredient(ItemID.BottledWater, 1);
-            reciple.needWater = true;
-            recipe.SetResult(ThoriumMod, "MarineRock", 50);
+            recipe.needWater = true;
+            recipe.SetResult(thoriumMod, "MarineRock", 50);
             recipe.AddRecipe();
+
+            // Recipe for brackish clump
+            recipe = new ModRecipe(this);
+            recipe.AddRecipeGroup("SkyblockThoriumHelper:BasicFish");
+            recipe.AddIngredient(ItemID.SandBlock, 25);
+            recipe.needWater = true;
+            recipe.SetResult(thoriumMod, "BrackMud", 25);
+            recipe.AddRecipe();
+
         }
     }
 }
