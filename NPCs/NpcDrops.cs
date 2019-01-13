ac775d67468b9fcdf0d2d52b6f0ab714d37b67e2
Stephen Cowley <StephenCowley44@gmail.com>
2019-01-12 22:14:35 -0700

    Fixed magma ore drop height

--- NPCs/NpcDrops.cs
+++ NPCs/NpcDrops.cs
@@ -1,12 +1,14 @@
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
 
 namespace SkyblockThoriumHelper.NPCs {
     public class NpcDrops : GlobalNPC {
         public override void NPCLoot(NPC npc) {
             Mod thoriumMod = ModLoader.GetMod("ThoriumMod");
-            if ((int)npc.position.Y >= 1300){ //Specifically for this skyblcok
+            Point center = npc.Center.ToTileCoordinates();
+            if (center.Y >= Main.maxTilesY - 300){
                 if (Main.rand.Next(1) == 0){ // 50% chance
                     Item.NewItem(npc.getRect(),
                                  thoriumMod.ItemType("MagmaOre"),
